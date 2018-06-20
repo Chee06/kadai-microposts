212708b7f00ec7359899ed053f6ee640fa4eccae
chisato.ito <chisato.ito@rakuten.com>
2018-06-20 01:12:44 +0000

    mod show controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,22 @@ class UsersController extends Controller
         ]);
     }
 
+//show-------------------------------------------------------------------------------------------------
+    public function show($id)
+    {
+        $user = User::find($id);
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'microposts' => $microposts,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.show', $data);
+    }
+
 //followings--------------------------------------------------------------------------------------------    
     public function followings($id)
     {
